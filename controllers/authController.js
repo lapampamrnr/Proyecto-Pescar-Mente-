3cff59eed0c9c3aca487b5c83f8a7a836c52a367
Marcela Adriana Serrano <114832707+lapampamrnr@users.noreply.github.com>
2026-07-07 11:54:10 -0300

    Add registration and login routes in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,23 @@
 
+const register = async (req, res) => {
+
+    res.status(200).json({
+        success: true,
+        message: "Ruta de registro funcionando correctamente."
+    });
+
+};
+
+const login = async (req, res) => {
+
+    res.status(200).json({
+        success: true,
+        message: "Ruta de login funcionando correctamente."
+    });
+
+};
+
+module.exports = {
+    register,
+    login
+};
